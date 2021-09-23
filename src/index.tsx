f610a55693ca9628812f960244944e99a3c4f95d
Steve Bourgoin <stiv.aw@gmail.com>
2021-09-23 11:42:51 -0400

    Export missing components

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,6 @@
+export { Badge } from "./components/Badge";
 export { Button } from "./components/Button";
+export { Card } from "./components/Card";
+export { Input } from "./components/Input";
+export { Label } from "./components/Label";
+export { Text } from "./components/Text";
