c556ccea17f04ae2e47cff9f94a027bf2bd51aa1
Steve Bourgoin <stiv.aw@gmail.com>
2021-09-29 15:52:11 -0400

    Add Modal (beta)

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,6 @@ export { Button } from "./components/Button";
 export { Card } from "./components/Card";
 export { Input } from "./components/Input";
 export { Label } from "./components/Label";
+export { Modal } from "./components/Modal";
 export { Text } from "./components/Text";
+export { useModal } from "./hooks/useModal";
