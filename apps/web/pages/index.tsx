75277f2a9cc15e34504e15252809cf11ae25122e
Steve Bourgoin <stiv.aw@gmail.com>
2022-07-31 18:24:54 -0400

    Update index.tsx

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,9 @@ import { Button } from "@tadasana/core";
 export default function Web() {
   return (
     <div className="m-8 flex flex-col items-center justify-center gap-12">
-      <h1 className="text-4xl text-primary-500">Tadasana</h1>
+      <h1 className="text-4xl text-primary-100 bg-primary-900 p-4 rounded-lg">
+        Tadasana
+      </h1>
       <Button />
     </div>
   );
