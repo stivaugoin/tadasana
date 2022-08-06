2155dd060650b7263a42998d54d663c6b8e94029
Steve Bourgoin <stiv.aw@gmail.com>
2022-08-06 10:13:14 -0400

    Use Tailwind CLI instead of PostCSS

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,10 +3,7 @@ import { Button } from "@tadasana/core";
 export default function Web() {
   return (
     <div className="m-8 flex flex-col items-center justify-center gap-12">
-      <h1 className="text-4xl text-primary-100 bg-primary-900 p-4 rounded-lg">
-        Tadasana
-      </h1>
-      <Button />
+      <Button>Button</Button>
     </div>
   );
 }
