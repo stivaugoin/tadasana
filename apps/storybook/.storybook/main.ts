d910db575929442af75eabcb7d97a1d613faa52b
Steve Bourgoin <stiv.aw@gmail.com>
2022-08-06 10:13:28 -0400

    Install Storybook

---
+++ apps/storybook/.storybook/main.ts
@@ -0,0 +1,10 @@
+const config = {
+  stories: ["../stories/**/*.stories.tsx"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+};
+
+export default config;
