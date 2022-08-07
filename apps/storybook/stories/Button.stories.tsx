3924cfe22907b95a138b294daa2d1578f4b953d5
Steve Bourgoin <stiv.aw@gmail.com>
2022-08-07 16:18:08 -0400

    Install chromatic and deploy storybook on Vercel (#20)

--- apps/storybook/stories/Button.stories.tsx
+++ apps/storybook/stories/Button.stories.tsx
@@ -11,5 +11,17 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Base = Template.bind({});
 Base.args = {
   children: "Button",
-  variant: "normal",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  children: "Button",
+  size: "sm",
+  variant: "primary",
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  children: "Button",
+  size: "md",
 };
