5938017334e33a8cc26f8de5ea874350bac98bef
Steve Bourgoin <stiv.aw@gmail.com>
2023-09-17 19:00:23 -0400

    Remove Badge component, for now

--- src/components/Badge/Badge.tsx
+++
@@ -1,9 +0,0 @@
-interface BadgeProps {
-  label: string;
-  size?: "sm" | "md" | "lg";
-  variant?: "primary" | "default" | "success" | "danger" | "warning";
-}
-
-export function Badge({ label }: BadgeProps) {
-  return <div className="bg-primary-500">{label}</div>;
-}
